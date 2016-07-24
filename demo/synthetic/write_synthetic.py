bf7c685371d2979b126e905488381a85a9e62379
Trever Hines <treverhines@gmail.com>
2016-07-24 12:04:48 -0400

    did more work on error propagation
    Former-commit-id: 5a05dabb917597aebb441c306d36cb0a513ba8e9
    Former-commit-id: bb2fec6370d4abb9fa09e2155b71eb9cb345872e
    Former-commit-id: 5fdd434ea327fda4533925175ea245dedeab0961

--- demo/synthetic/write_synthetic.py
+++ demo/synthetic/write_synthetic.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import numpy as np
 import pygeons.ioconv
-import pygeons.decyear
+import pygeons.dateconv
 import rbf.halton
 
 Nx = 10
-time = pygeons.decyear.decyear_range('2000-01-01','2002-01-01',5,'%Y-%m-%d')
+start_time = pygeons.dateconv.decday('2000-01-01','%Y-%m-%d')
+stop_time = pygeons.dateconv.decday('2001-01-01','%Y-%m-%d')
+time = np.arange(start_time,stop_time+1)
+
 Nt = len(time)
 pos = 2*np.pi*(rbf.halton.halton(Nx,2) - 0.5)
 lon = pos[:,0] - 84.5
 lat = pos[:,1] + 43.0
 
-east = 100*np.sin(lon[None,:])*np.cos(lat[None,:])*np.cos(time[:,None])
-north = 100*np.cos(lon[None,:])*np.sin(lat[None,:])*np.cos(time[:,None])
-vertical = 100*np.cos(lon[None,:])*np.cos(lat[None,:])*np.sin(time[:,None])
+east = 100*np.sin(lon[None,:])*np.cos(lat[None,:])*np.cos(time[:,None]/365.25)
+north = 100*np.cos(lon[None,:])*np.sin(lat[None,:])*np.cos(time[:,None]/365.25)
+vertical = 100*np.cos(lon[None,:])*np.cos(lat[None,:])*np.sin(time[:,None]/365.25)
 
 east_std = 0.0*np.ones((Nt,Nx))
 north_std = 0.0*np.ones((Nt,Nx))
