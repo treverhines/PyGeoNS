0210fc88f2b50fd0e21984b3c879b6fbaeba824d
hinest <treverhines@gmail.com>
2016-07-26 22:23:24 -0400

    pygeons now does interpolation
    Former-commit-id: 6722ecfb3121803bb40879b4531576b8a9ed5dbe
    Former-commit-id: 521cbbbee64dd02637fd644bd3d8605d1bd65e35
    Former-commit-id: 795fef55c2125369c7d390abbffae574f87cb480

--- demo/junk/test_clean.py
+++ demo/junk/test_clean.py
@@ -4,10 +4,10 @@ import pygeons.clean
 import matplotlib.pyplot as plt
 import logging
 import pygeons.diff
-import pygeons.cuts
+import pygeons.smooth
 logging.basicConfig(level=logging.INFO)
 
-Nt = 1000
+Nt = 100
 Nx = 1
 S = 0.1
 t = np.linspace(0.0,5.0,Nt)
@@ -15,16 +15,21 @@ x = np.random.random((Nx,2))
 u = 1*np.sin(2*t)[:,None]
 u = u.repeat(Nx,axis=1)
 u += np.random.normal(0.0,S,(Nt,Nx))
+
+
+mask = np.zeros((Nt,Nx),dtype=bool)
+mask[(t>2) & (t<4.5)] = True
+
 sigma = S*np.ones((Nt,Nx))
+sigma[mask] = np.inf
+u[mask] = np.nan
 
-tc = pygeons.cuts.TimeCuts([1.6])
+#tc = pygeons.cuts.TimeCuts([1.6])
 ds = pygeons.diff.acc()
-ds['time']['cuts'] = tc
-sigma[t > 1.5] = np.inf
-us,perts = pygeons.smooth.network_smoother(t,x,
-                                           u=u[:,:,0],v=u[:,:,1],z=u[:,:,2],
-                                           su=sigma[:,:,0],sv=sigma[:,:,1],sz=sigma[:,:,2]) 
-
+#ds['time']['cuts'] = tc
+#sigma[t > 1.5] = np.inf
+us = pygeons.smooth.smooth(t,x,u,sigma=sigma,diff_specs=[ds],fill='interpolate')
+print(us)
 plt.plot(t,u[:,0],'ko')
 plt.plot(t,us[:,0],'b-')
 plt.show()
