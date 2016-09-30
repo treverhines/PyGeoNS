4c32c41d0422008f4b619bfe4c43b52bd3722d64
hinest <treverhines@gmail.com>
2016-09-30 17:09:08 -0400

    added another demonstration
    Former-commit-id: f456f4870a82fff9ad9e4125da942cb69589da05
    Former-commit-id: 54c06b1724bbed1a3d92ed86d4173f14e734da9f
    Former-commit-id: 79c2dc9dd8e5c9eefccd67163616beb1522a3751

--- demo/spatial_smoothing/run.sh
+++ demo/spatial_smoothing/run.sh
@@ -1,33 +1,31 @@
-# Smoothing Synthetic Cascadia Data
+# PYGEONS DEMONSTRATION #1:
+# SPATIALLY SMOOTHING SYNTHETIC CASCADIA DATA
 
-# This script demonstrates how to use pygeons to spatially smooth GPS
-# data.
+# This script demonstrates how to spatially smooth GPS data.  We use
+# the synthetic data saved in synthetic.csv. The underlying signal in
+# the synthetic data consists of sinusoids with a 400km wavelength and
+# we obscure this signal with white noise.
 
-# convert the csv file to and HDF5 file. The first and second
+# Convert the csv file to and HDF5 file. The first and second
 # positional arguments are the file name and file type respectively.
 # Possible file types are csv, pbo_csv, and pbo_pos
 pygeons-toh5 synthetic.csv csv -o work/synthetic.h5
 
 # spatial cutoff frequency in meters^-1. The underlying signal in the
-# synthetic data has a frequency of 2.5e-6.  If we pick a cutoff
-# frequency which is just above the frequency of the signals then the
-# underlying signal will be kept in tact.
+# synthetic data has a frequency of 2.5e-6.  If we pick a slightly
+# larger cutoff frequency then the underlying signal will be kept in
+# tact.
 SPATIAL_CUTOFF=5e-6
 pygeons-sfilter work/synthetic.h5 --cutoff $SPATIAL_CUTOFF
 
-echo "spawning three subprocesses to view the observed, smoothed and true data sets"
-# view the data set before smoothing
-pygeons-view work/synthetic.h5 --image_clim -15 15\
-             --image_array_size 1000 --map_title "data" &
+# Compare the synthetic data to the smoothed data
+pygeons-view work/synthetic.h5 work/synthetic.sfilter.h5 --image_clim -15 15\
+             --image_array_size 1000 --data_set_labels synthetic smoothed
 
-# view the data set after smoothing
-pygeons-view work/synthetic.sfilter.h5 --image_clim -15 15\
-             --image_array_size 1000 --map_title "smoothed" &
-
-# view the true signal
+# Compare the smoothed data to the underlying signal
 pygeons-toh5 synthetic.nonoise.csv csv -o work/synthetic.nonoise.h5
-pygeons-view work/synthetic.nonoise.h5 --image_clim -15 15\
-             --image_array_size 1000 --map_title "true signal" &
+pygeons-view work/synthetic.nonoise.h5 work/synthetic.sfilter.h5 --image_clim -15 15\
+             --image_array_size 1000 --data_set_labels true smoothed
 
 
 
