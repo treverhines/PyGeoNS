2ad849a26efdf80ba30d37b50306ff0fbc1456cc
hinest <treverhines@gmail.com>
2016-11-21 19:45:55 -0500

    added more
    Former-commit-id: 893aa9eab60174934c4cfef9e4059952ad8ab4b8
    Former-commit-id: 87ebb416c304494407ead7b8c28116787b81a5b4
    Former-commit-id: e3d5685cbfa961614113b94396108317a38cb8c9

--- demo/parkfield/run.sh
+++ demo/parkfield/run.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # coordinates of first endpoint of dislocation
-lons1="-120.566"
-lats1="36.0"
-# coordinates of second endpoint of dislocation
-lons2="-120.357"
-lats2="35.8349"
+lons="-120.566 -120.357"
+lats="36.0 35.8349"
+conn="0-1"
 
-pygeons-convert data.csv work/data.h5
+pygeons-toh5 data.csv pbo_csv -o work/data.h5
 
-pygeons-downsample work/data.h5 1 \
+pygeons-downsample work/data.h5 \
   --start_date 2004-07-01 --stop_date 2005-07-01 \
-  --cut_dates 2004-09-29 -vv
+  --break_dates 2004-09-29 -vv
 
-pygeons-zero work/data.downsample.h5 2004-09-01 7
+pygeons-tfilter work/data.downsample.h5 --cutoff 0.02 --diff 1 \
+  --break_dates 2004-09-29 \
+  --procs 6 -vv
 
-pygeons-smooth work/data.downsample.zero.h5 --time_scale 50.0 --length_scale 10000 \
-  --cut_dates 2004-09-29 \
-  --cut_endpoint1_lons $lons1 \
-  --cut_endpoint1_lats $lats1 \
-  --cut_endpoint2_lons $lons2 \
-  --cut_endpoint2_lats $lats2 -vv
+pygeons-sfilter work/data.downsample.tfilter.h5 --cutoff 1e-4 \
+  --break_lons $lons \
+  --break_lats $lats \
+  --break_conn $conn \
+  --procs 6 -vv
 
-pygeons-view work/data.downsample.zero.smooth.h5 -vv
+pygeons-view work/data.downsample.tfilter.sfilter.h5 work/data.downsample.tfilter.h5 \
+  --break_lons $lons \
+  --break_lats $lats \
+  --break_conn $conn \
+  -vv
 
