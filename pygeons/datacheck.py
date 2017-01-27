aadef2664d80b98ba9e980f53cf76330d7bf2576
Trever Hines <treverhines@gmail.com>
2017-01-27 08:24:21 -0500

    started a readme
    Former-commit-id: 79e467a95382c5999537725034da232dd36eaafc
    Former-commit-id: 92e4b796d169cff01bcd92d46c87bcb948a65757
    Former-commit-id: a8151a1ac5b8be7dde28cf1792b56f9114dd582f

--- pygeons/datacheck.py
+++ pygeons/datacheck.py
@@ -1,45 +1,100 @@
-#!/usr/bin/env python
+''' 
+
+This module contains functions that check the consistency of data 
+dictionaries. A valid dictionary will contain the following entries
+
+  time : (Nt,) array
+    Array of observation times. These are integer values of modified 
+    Julian dates. Days are used instead of years because there is no 
+    ambiguity about the length of a day
+        
+  id : (Nx,) array
+    Array of 4-character IDs for each station
+
+  longitude : (Nx,) array
+    Longitude for each station in degrees
+        
+  latitude : (Nx,) array
+    Latitude for each station in degrees
+
+  east,north,vertical : (Nt,Nx) array
+    data components. The units should be in terms of meters and days 
+    and should be consistent with the values specified for 
+    *space_exponent* and *time_exponent*. For example, if 
+    *time_exponent* is -1 and *space_exponent* is 1 then the units 
+    should be in meters per day.
+
+  east_std,north_std,vertical_std : (Nt,Nx) array
+    One standard deviation uncertainty. These should have the same 
+    units as the data components
+
+  time_exponent : int
+    Indicates the power of the time units for the data. -1 
+    indicates that the data is a rate, -2 is an acceleration etc.
+
+  space_exponent : int
+    Indicates the power of the spatial units for the data
+
+'''
 import copy
 import numpy as np
 import logging
 logger = logging.getLogger(__name__)
+
+class DataError(Exception):
+  ''' 
+  An error indicating that the is data dictionary inconsistent.
+  '''
+  pass
+
+
+def check_entries(data):
+  ''' 
+  Checks if all the entries exist in the data dictionary
+  '''
+  keys = ['time','id','longitude','latitude','east','north',
+          'vertical','east_std','north_std','vertical_std',
+          'time_exponent','space_exponent']
+  for k in keys:
+    if not self.has_key(k):
+      raise DataError('Data dictionary is missing *%s*' % k)
+  
+
+def check_shapes(data):
+  ''' 
+  Checks if the shapes of each entry are consistent.
+  '''
+  # check for proper sizes
+  Nt = self['time'].shape[0]
+  Nx = self['id'].shape[0]
+  keys = ['longitude','latitude']
+  for k in keys:     
+    if not self[k].shape == (Nx,):
+      raise DataError('*%s* has shape %s but it should have shape %s' 
+                      % (k,self[k].shape,(Nx,)))
+  
+  keys = ['east','north','vertical','east_std','north_std','vertical_std']
+  for k in keys:     
+    if not self[k].shape == (Nt,Nx):
+      raise DataError('*%s* has shape %s but it should have shape %s' 
+                      % (k,self[k].shape,(Nt,Nx)))
+
+
+def check_positive_uncertainties(data):
+  ''' 
+  Checks if all the uncertainties are positive.
+  '''
+  keys = ['east_std','north_std','vertical_std']
+  for k in keys:
+    if np.any(self[k] <= 0.0):
+      raise DataError('*%s* contains zeros or negative values' % k)
+     
 
 class DataDict(dict):
   ''' 
   Specialized dictionary for GPS data.  An instance of this class 
   should contain the following entries
   
-      time : (Nt,) array
-        Array of observation times. These are in units of days since 
-        1970-01-01. Days are used instead of years because there is no 
-        ambiguity about the length of a day
-        
-      id : (Nx,) array
-        Array of 4-character IDs for each station
-
-      longitude : (Nx,) array
-        Longitude for each station in degrees
-        
-      latitude : (Nx,) array
-        Latitude for each station in degrees
-
-      east,north,vertical : (Nt,Nx) array
-        data components. The units should be in terms of meters and 
-        days and should be consistent with the values specified for 
-        *space_exponent* and *time_exponent*. For example, if *time_exponent* 
-        is -1 and *space_exponent* is 1 then the units should be in 
-        meters per day.
-
-      east_std,north_std,vertical_std : (Nt,Nx) array
-        One standard deviation uncertainty. These should have the same 
-        units as the data components
-
-      time_exponent : int
-        Indicates the power of the time units for the data. -1 
-        indicates that the data is a rate, -2 is an acceleration etc.
-
-      space_exponent : int
-        Indicates the power of the spatial units for the data
   
   Calling the function *check_self_consistency* will verify that all 
   the entries exist and have the proper shapes 
@@ -147,7 +202,7 @@ class DataDict(dict):
     for k in keys:
       if np.any(self[k] < 0.0):
         raise ValueError(
-          'found zero or negative uncertainty for *%s*' % k)
+          'found negative uncertainty for *%s*' % k)
      
     # make sure there are no duplicate station IDs
     if len(np.unique(self['id'])) != len(self['id']):
