made progress. still need to fix units issue

pygeons/main/gprocs.py
@@ -32,8 +32,43 @@ def set_units(units):
   return decorator  
 
 
+@set_units([])
+def null():
+  '''Null GaussianProcess'''
+  return gauss.GaussianProcess(gauss._zero_mean,
+                               gauss._zero_covariance,
+                               basis=gauss._empty_basis)
+                               
 # 1D GaussianProcess constructors
 #####################################################################
+@set_units([])
+def p0_1d():
+  ''' 
+  Returns a *GaussianProcess* with annual and semiannual sinusoid
+  basis functions. The coefficients have zero mean and unit std. dev.
+  '''
+  def basis(x,diff):
+    powers = np.array([[0]])
+    out = rbf.poly.mvmonos(x,powers,diff)
+    return out
+
+  return gauss.gpbfci(basis,dim=1)  
+
+
+@set_units([])
+def p1_1d():
+  ''' 
+  Returns a *GaussianProcess* with annual and semiannual sinusoid
+  basis functions. The coefficients have zero mean and unit std. dev.
+  '''
+  def basis(x,diff):
+    powers = np.array([[1]])
+    out = rbf.poly.mvmonos(x,powers,diff)
+    return out
+
+  return gauss.gpbfci(basis,dim=1)  
+
+
 def per_1d():
   ''' 
   Returns a *GaussianProcess* with annual and semiannual sinusoid
@@ -50,6 +85,23 @@ def per_1d():
   mu = np.zeros(4)
   sigma = np.ones(4)
   return gauss.gpbfc(basis,mu,sigma,dim=1)
+
+
+@set_units([])
+def peri_1d():
+  ''' 
+  Returns a *GaussianProcess* with annual and semiannual sinusoid
+  basis functions. The coefficients have zero mean and unit std. dev.
+  '''
+  def basis(x):
+    # no derivatives because im lazy
+    out = np.array([np.sin(2*np.pi*x[:,0]/365.25),
+                    np.cos(2*np.pi*x[:,0]/365.25),
+                    np.sin(4*np.pi*x[:,0]/365.25),
+                    np.cos(4*np.pi*x[:,0]/365.25)]).T
+    return out
+
+  return gauss.gpbfci(basis,dim=1)
 
 
 def step_1d(t0):
@@ -71,6 +123,26 @@ def step_1d(t0):
   mu = np.zeros(1)
   sigma = np.ones(1)
   return gauss.gpbfc(basis,mu,sigma,dim=1)
+
+
+@set_units(['mjd'])
+def stepi_1d(t0):
+  ''' 
+  Returns a *GaussianProcess* with a heaviside function centered at
+  *t0*. The size of the step has zero mean and unit std. dev.
+  '''
+  def basis(x,diff):
+    if diff == (0,):
+      out = (x[:,0] >= t0).astype(float)
+    else:
+      # derivative is zero everywhere (ignore the step at t0)
+      out = np.zeros(x.shape[0],dtype=float)  
+    
+    # turn into an (N,1) array
+    out = out[:,None]
+    return out
+
+  return gauss.gpbfci(basis,dim=1)
 
 
 def ramp_1d(t0):
@@ -128,6 +200,7 @@ def exp_1d(sigma,cls):
   return gauss.gpexp((0.0,sigma**2,cls),dim=1)
 
 
+@set_units(['mm/yr^0.5','yr^-1'])
 def fogm_1d(s,fc):
   ''' 
   Returns a *GaussianProcess* describing an first-order Gauss-Markov
@@ -149,6 +222,7 @@ def fogm_1d(s,fc):
   return gauss.gpexp((0.0,coeff,cls),dim=1)
 
 
+@set_units(['mm/yr^0.5','mjd'])
 def bm_1d(w,t0):
   ''' 
   Returns brownian motion with scale parameter *w* and reference time
@@ -267,22 +341,13 @@ def kernel_product(gp1,gp2):
 
 
 @set_units([])
-def null():
-  '''Null GaussianProcess'''
-  return gauss.GaussianProcess(gauss._zero_mean,
-                               gauss._zero_covariance,
-                               basis=gauss._empty_basis,
-                               dim=3)
-                               
-
-@set_units([])
-def p11():
+def p10_3d():
   ''' 
   Gaussian process with polynomial basis functions (1,0,0), (1,1,0),
   and (1,0,1)
   '''
   def basis(x,diff):
-    powers = np.array([[1,0,0],[1,1,0],[1,0,1]])
+    powers = np.array([[1,0,0]])
     out = rbf.poly.mvmonos(x,powers,diff)
     return out
 
@@ -290,14 +355,41 @@ def p11():
 
 
 @set_units([])
-def p21():
+def p11_3d():
+  ''' 
+  Gaussian process with polynomial basis functions (1,0,0), (1,1,0),
+  and (1,0,1)
+  '''
+  def basis(x,diff):
+    powers = np.array([[1,1,0],[1,0,1]])
+    out = rbf.poly.mvmonos(x,powers,diff)
+    return out
+
+  return gauss.gpbfci(basis,dim=3)  
+
+
+@set_units([])
+def p20_3d():
   ''' 
   Gaussian process with polynomial basis functions (1,0,0), (1,1,0),
   (1,0,1), (2,0,0), (2,1,0), and (2,0,1).
   '''
   def basis(x,diff):
-    powers = np.array([[1,0,0],[1,1,0],[1,0,1],
-                       [2,0,0],[2,1,0],[2,0,1]])
+    powers = np.array([[2,0,0]])
+    out = rbf.poly.mvmonos(x,powers,diff)
+    return out
+  
+  return gauss.gpbfci(basis,dim=3)  
+
+
+@set_units([])
+def p21_3d():
+  ''' 
+  Gaussian process with polynomial basis functions (1,0,0), (1,1,0),
+  (1,0,1), (2,0,0), (2,1,0), and (2,0,1).
+  '''
+  def basis(x,diff):
+    powers = np.array([[2,1,0],[2,0,1]])
     out = rbf.poly.mvmonos(x,powers,diff)
     return out
   
@@ -305,7 +397,7 @@ def p21():
   
 
 @set_units(['mm','yr','km'])
-def se_se(a,b,c):
+def se_se_3d(a,b,c):
   # convert hyperparameter units to m and days
   a *= 1.0/1000.0 # mm to m
   b *= 365.25 # yr to day
@@ -317,7 +409,7 @@ def se_se(a,b,c):
 
 
 @set_units(['mm*yr^-0.5','yr^-1','km'])
-def fogm_se(a,b,c):
+def fogm_se_3d(a,b,c):
   # convert hyperparameter units to m and days
   a *= (1.0/1000.0)*(365.25**-0.5) # mm*yr^-0.5 to m*day^-0.5
   b *= 365.25**-1 # yr^-1 to day^-1
@@ -329,7 +421,7 @@ def fogm_se(a,b,c):
 
 
 @set_units(['mm*yr^-0.5','mjd','km'])
-def bm_se(a,b,c):
+def bm_se_3d(a,b,c):
   # convert hyperparameter units to m and days
   a *= (1.0/1000.0)*(365.25**-0.5) # mm*yr^-1.5 to m*day^-1.5
   c *= 1000.0 # km to m
@@ -340,7 +432,7 @@ def bm_se(a,b,c):
 
 
 @set_units(['mm*yr^-1.5','mjd','km'])
-def ibm_se(a,b,c):
+def ibm_se_3d(a,b,c):
   # convert hyperparameter units to m and days
   a *= (1.0/1000.0)*(365.25**-1.5) # mm*yr^-1.5 to m*day^-1.5
   c *= 1000.0 # km to m
@@ -351,7 +443,7 @@ def ibm_se(a,b,c):
 
 
 @set_units(['mm','yr','km'])
-def mat32_se(a,b,c):
+def mat32_se_3d(a,b,c):
   # convert hyperparameter units to m and days
   a *= 1.0/1000.0 # mm to m
   b *= 365.25 # yr to day
@@ -363,7 +455,7 @@ def mat32_se(a,b,c):
 
 
 @set_units(['mm','yr','km'])
-def mat52_se(a,b,c):
+def mat52_se_3d(a,b,c):
   # convert hyperparameter units to m and days
   a *= 1.0/1000.0 # mm to m
   b *= 365.25 # yr to day
@@ -375,7 +467,7 @@ def mat52_se(a,b,c):
 
 
 @set_units(['mm','km'])
-def per_se(a,b):
+def per_se_3d(a,b):
   # convert hyperparameter units to m and days
   a *= 1.0/1000.0 # mm to m
   b *= 365.25 # yr to day
@@ -386,7 +478,7 @@ def per_se(a,b):
 
 
 @set_units(['mm','km'])
-def step_se(a,b):
+def step_se_3d(a,b):
   # convert hyperparameter units to m and days
   a *= 1.0/1000.0 # mm to m
   b *= 365.25 # yr to day
@@ -397,7 +489,7 @@ def step_se(a,b):
 
 
 @set_units(['mm','km'])
-def ramp_se(a,b):
+def ramp_se_3d(a,b):
   # convert hyperparameter units to m and days
   a *= 1.0/1000.0 # mm to m
   b *= 365.25 # yr to day
@@ -410,38 +502,46 @@ def ramp_se(a,b):
 # create a dictionary of all Gaussian process constructors in this
 # module
 CONSTRUCTORS = {'null':null,
-                'p11':p11,
-                'p21':p21,
-                'se*se':se_se,
-                'fogm*se':fogm_se,
-                'bm*se':bm_se,
-                'ibm*se':ibm_se,
-                'mat32*se':mat32_se,
-                'mat52*se':mat52_se,
-                'per*se':per_se,
-                'step*se':step_se,
-                'ramp*se':ramp_se}
+                'p0':p0_1d, 
+                'p1':p1_1d, 
+                'per':peri_1d, 
+                'step':stepi_1d,
+                'fogm':fogm_1d,
+                'bm':bm_1d,
+                'p10':p10_3d,
+                'p11':p11_3d,
+                'p20':p20_3d,
+                'p21':p21_3d,
+                'se-se':se_se_3d,
+                'fogm-se':fogm_se_3d,
+                'bm-se':bm_se_3d,
+                'ibm-se':ibm_se_3d,
+                'mat32-se':mat32_se_3d,
+                'mat52-se':mat52_se_3d,
+                'per-se':per_se_3d,
+                'step-se':step_se_3d,
+                'ramp-se':ramp_se_3d}
   
-def gpcomp(model,args):
+def gpcomp(models,args):
   ''' 
   Returns a composite Gaussian process. The components are specified
   with the *model* string, and the arguments for each component are
   specied with *args*. For example,
   
-  >>> gpcomp('fogm+se',(1.0,2.0,3.0,4.0)) 
+  >>> gpcomp(['fogm','se'],[1.0,2.0,3.0,4.0]) 
   
   creates a GaussianProcess composed of a FOGM and an SE model. The
   first two arguments are passed to *gpfogm* and the second two
   arguments are passed to *se*
   '''
   args = list(args)
-  models = model.strip().split('+')
+  models = list(models)
   cs = [CONSTRUCTORS[m] for m in models] # constructor for each component
   n  = sum(ci.n for ci in cs)
   if len(args) != n:
     raise ValueError(
       '%s parameters were specified for the model "%s", but it '
-      'requires %s parameters.\n' %(len(args),model,n))
+      'requires %s parameters.\n' %(len(args),' '.join(models),n))
   
   gp = null()
   for ci in cs:
@@ -450,11 +550,11 @@ def gpcomp(model,args):
   return gp
   
 
-def get_units(model):
+def get_units(models):
   ''' 
   returns the units for the GaussianProcess parameters
   '''
-  models = model.strip().split('+')
+  models = list(models)
   cs = [CONSTRUCTORS[m] for m in models]
   units = []
   for ci in cs:

pygeons/main/main.py
@@ -52,7 +52,6 @@ def _params_dict(b):
            'north':arr,
            'vertical':arr}
 
-  print(out)
   return out
 
 
@@ -133,12 +132,13 @@ def pygeons_reml(input_file,model,params,fix=(),
       fout.flush()
 
 
-def pygeons_strain(input_file,prior_model,prior_params,
+def pygeons_strain(input_file,
+                   prior_model=('se-se',),prior_params=(1.0,0.05,50.0),
+                   noise_model=(),noise_params=(),
+                   station_noise_model=('p0',),station_noise_params=(),
                    start_date=None,stop_date=None,positions=None,
-                   noise_model='null',noise_params=(),
-                   return_sample=False,
                    outlier_tol=4.0,
-                   output_file=None):
+                   output_dir=None):
   ''' 
   Performs temporal Gaussian process regression.
   
@@ -165,9 +165,6 @@ def pygeons_strain(input_file,prior_model,prior_params,
   noise_params : float array
     Hyperparameters for the noise
 
-  return_sample : bool, optional
-    Return a sample of the posterior.
-
   procs : int, optional
     Number of subprocesses to spawn.
 
@@ -183,13 +180,15 @@ def pygeons_strain(input_file,prior_model,prior_params,
     raise ValueError('input dataset must have units of displacement')
     
   logger.info('Performing spatial Gaussian process regression ...')
-  out = dict((k,np.copy(v)) for k,v in data.iteritems())
+  out_rm = dict((k,np.copy(v)) for k,v in data.iteritems())
+  out_res = dict((k,np.copy(v)) for k,v in data.iteritems())
   out_dx = dict((k,np.copy(v)) for k,v in data.iteritems())
   out_dy = dict((k,np.copy(v)) for k,v in data.iteritems())
 
   # convert params to a dictionary of hyperparameters for each direction
   prior_params = _params_dict(prior_params)
   noise_params = _params_dict(noise_params)
+  station_noise_params = _params_dict(station_noise_params)
 
   bm = make_basemap(data['longitude'],data['latitude'])
   x,y = bm(data['longitude'],data['latitude'])
@@ -222,31 +221,31 @@ def pygeons_strain(input_file,prior_model,prior_params,
   
   # convert the domain units from m to km
   for dir in ['east','north','vertical']:
-    u,su,dx,sdx,dy,sdy  = strain(data['time'],
-                                 xy,
-                                 data[dir],
-                                 data[dir+'_std_dev'],
-                                 prior_model,prior_params[dir],
-                                 out_t=output_time,
-                                 out_x=output_xy,
-                                 noise_model=noise_model,
-                                 noise_params=noise_params[dir],
-                                 tol=outlier_tol,
-                                 return_sample=return_sample)
+    removed,fit,dx,sdx,dy,sdy  = strain(data['time'][:,None],
+                                        xy,
+                                        data[dir],
+                                        data[dir+'_std_dev'],
+                                        prior_model=prior_model,
+                                        prior_params=prior_params[dir],
+                                        noise_model=noise_model,
+                                        noise_params=noise_params[dir],
+                                        station_noise_model=station_noise_model,
+                                        station_noise_params=station_noise_params[dir],
+                                        out_t=output_time[:,None],
+                                        out_x=output_xy,
+                                        tol=outlier_tol)
     # convert back to m and days
-    out[dir] = u
-    out[dir+'_std_dev'] = su
+    out_rm[dir][~removed] = np.nan
+    out_rm[dir+'_std_dev'][~removed] = np.inf
+    out_res[dir] = data[dir] - fit
+    out_res[dir][removed] = np.nan
+    out_res[dir+'_std_dev'][removed] = np.inf
     out_dx[dir] = dx
     out_dx[dir+'_std_dev'] = sdx
     out_dy[dir] = dy
     out_dy[dir+'_std_dev'] = sdy
 
   # set the new lon lat and id if positions was given
-  out['time'] = output_time
-  out['longitude'] = output_lon
-  out['latitude'] = output_lat
-  out['id'] = output_id
-
   out_dx['time'] = output_time
   out_dx['longitude'] = output_lon
   out_dx['latitude'] = output_lat
@@ -262,12 +261,17 @@ def pygeons_strain(input_file,prior_model,prior_params,
   out_dy['space_exponent'] = 0
 
   # write to output file
-  if output_file is None:
-    output_file = _change_extension(input_file,'strain.h5')
+  if output_dir is None:
+    output_dir = _change_extension(input_file,'strain')
 
-  output_dx_file = _change_extension(output_file,'dx.h5')
-  output_dy_file = _change_extension(output_file,'dy.h5')
-  hdf5_from_dict(output_file,out)
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+  
+  output_rm_file = output_dir + '/removed.h5'
+  output_res_file = output_dir + '/residuals.h5'
+  output_dx_file = output_dir + '/xdiff.h5'
+  output_dy_file = output_dir + '/ydiff.h5'
+  hdf5_from_dict(output_rm_file,out_rm)
+  hdf5_from_dict(output_res_file,out_res)
   hdf5_from_dict(output_dx_file,out_dx)
   hdf5_from_dict(output_dy_file,out_dy)
-  

pygeons/main/strain.py
@@ -5,7 +5,6 @@ specialized for PyGeoNS
 import numpy as np
 import logging
 import rbf.gauss
-from pygeons.mp import parmap
 from pygeons.main.gprocs import gpcomp
 logger = logging.getLogger(__name__)
 
@@ -22,40 +21,57 @@ def _is_outlier(d,s,sigma,mu,p,tol):
   p : (N,P) prior basis functions
   tol : outlier tolerance
   
-  returns a boolean array indicating which points are outliers
+  returns a boolean array indicating which points are outliers and the
+  best fit trend to the data.
   '''
+  itr = 1
   out = np.zeros(d.shape[0],dtype=bool)
   while True:
-    q = sum(~out)
-    a = sigma[np.ix_(~out,~out)] + np.diag(s[~out]**2)
-    b = rbf.gauss._cholesky_block_inv(a,p[~out])
+    logger.debug('Starting iteration %s of outlier detection routine' % itr)
+    mask = np.isinf(s) | out
+    q = sum(~mask)
+    a = sigma[np.ix_(~mask,~mask)] + np.diag(s[~mask]**2)
+    b = rbf.gauss._cholesky_block_inv(a,p[~mask])
     c = np.empty((d.shape[0],b.shape[0]))
-    c[:,:q] = sigma[:,~out]
+    c[:,:q] = sigma[:,~mask]
     c[:,q:] = p
     r = np.empty(b.shape[0])
-    r[:q] = d[~out] - mu[~out]
+    r[:q] = d[~mask] - mu[~mask]
     r[q:] = 0.0
-    pred = mu + c.dot(b).dot(r)
-    res = np.abs(pred - d)/s
-    rms = np.sqrt(np.mean(res[~out]**2))
-    if np.all(out == (res > tol*rms)):
+    fit = mu + c.dot(b).dot(r)
+    res = np.abs(fit - d)/s
+    res[np.isinf(s)] = np.inf
+    rms = np.sqrt(np.mean(res[~mask]**2))
+    if np.all(mask == (res > tol*rms)):
       break
 
     else:
-      out = (res > tol*rms)
+      out = (res > tol*rms) & ~np.isinf(s)
+      itr += 1
 
-  return out
+  logger.debug('Detected %s outliers out of %s observations' % (sum(out),sum(~np.isinf(s))))
+  return out,fit
 
 
-def strain(t,x,d,s,
-           prior_model,prior_params,
+def _remove_zero_columns(A):
+  ''' 
+  remove columns of *A* which only have zeros
+  '''
+  toss = np.all(A == 0.0,axis=0)
+  A = A[:,~toss]
+  return A
+
+
+def strain(t,x,d,sd,
+           prior_model=('se-se',),
+           prior_params=(1.0,0.05,50.0),
+           noise_model=('null',),
+           noise_params=(),
+           station_noise_model=('p0',),
+           station_noise_params=(),
            out_t=None,
            out_x=None,
-           noise_model='null',
-           noise_params=(),
            tol=4.0,
-           procs=0,
-           return_sample=False,
            offsets=True):
   ''' 
   Computes deformation gradients from displacement data.
@@ -71,7 +87,7 @@ def strain(t,x,d,s,
   d : (Nt,Nx) array
     Grid of observations at time *t* and position *x*. 
   
-  s : (Nt,Nx) array
+  sd : (Nt,Nx) array
     Grid of observation uncertainties
   
   prior_model : str
@@ -100,93 +116,123 @@ def strain(t,x,d,s,
   tol : float, optional
     Tolerance for the outlier detection algorithm.
     
-  procs : int, optional
-    Distribute the tasks among this many subprocesses. 
-  
-  return_sample : bool, optional
-    If True then *out_mean* is a sample of the posterior.
+  Returns
+  -------
+  removed: (Nt,Nx) bool array
+    array of data that has been removed 
 
+  fit: (Nt,Nx) float array
+    array of fit to the data
+
+  dx: (Mt,Mx) array
+    array of x derivatives    
+
+  sigma_dx: (Mt,Mx) array
+    array of x derivative uncertainties    
+
+  dy: (Mt,Mx) array
+    array of y derivatives    
+
+  sigma_dy: (Mt,Mx) array
+    array of y derivative uncertainties    
+    
   '''  
   t = np.asarray(t,dtype=float)
   x = np.asarray(x,dtype=float)
-  d = np.asarray(d,dtype=float)
-  s = np.asarray(s,dtype=float)
+  d = np.array(d,dtype=float,copy=True)
+  sd = np.array(sd,dtype=float,copy=True)
+  Nt,Nx = t.shape[0],x.shape[0]
+  # allocate array indicating which data have been removed
+  removed = np.zeros((Nt,Nx),dtype=bool)
   if out_t is None:
     out_t = t
 
   if out_x is None:
     out_x = x
 
-  # Toss stations with no observations.
-  toss = np.all(np.isinf(s),axis=0) 
-  x = x[~toss]
-  d = d[:,~toss]
-  s = s[:,~toss]
+  prior_gp = gpcomp(prior_model,prior_params)
+  noise_gp = gpcomp(noise_model,noise_params)    
+  sta_gp   = gpcomp(station_noise_model,station_noise_params)
 
-  d = d.flatten()
-  s = s.flatten()
   t_grid,x0_grid = np.meshgrid(t,x[:,0],indexing='ij')  
   t_grid,x1_grid = np.meshgrid(t,x[:,1],indexing='ij')  
   # flat observation times and positions
-  z = np.array([t_grid.flatten(),
-                x0_grid.flatten(),
-                x1_grid.flatten()]).T
+  z = np.array([t_grid.ravel(),
+                x0_grid.ravel(),
+                x1_grid.ravel()]).T
 
   t_grid,x0_grid = np.meshgrid(out_t,out_x[:,0],indexing='ij')  
   t_grid,x1_grid = np.meshgrid(out_t,out_x[:,1],indexing='ij')  
   # flat observation times and positions
-  out_z = np.array([t_grid.flatten(),
-                    x0_grid.flatten(),
-                    x1_grid.flatten()]).T
+  out_z = np.array([t_grid.ravel(),
+                    x0_grid.ravel(),
+                    x1_grid.ravel()]).T
 
-  # create basis functions for station offsets
-  p = np.zeros((t.shape[0],x.shape[0],x.shape[0]))
-  for i in range(x.shape[0]): p[:,i,i] = 1.0
-  p = p.reshape((t.shape[0]*x.shape[0],x.shape[0]))
+  # create covariance matrix and basis functions describing noise.
+  # start with station-specific noise.
+  sta_sigma = sta_gp.covariance(t,t)
+  sta_p = sta_gp.basis(t)
+  Np = sta_p.shape[1]
+
+  # If a station has insufficient data for the station basis vectors
+  # then mask whatever few data points it does have 
+  bad_stations_bool = np.sum(~np.isinf(sd),axis=0) < Np
+  bad_stations,  = np.nonzero(bad_stations_bool)
+  good_stations, = np.nonzero(~bad_stations_bool) 
+  d[:,bad_stations] = np.nan
+  sd[:,bad_stations] = np.inf
+  removed[:,bad_stations] = True
   
-  # if the uncertainty is inf then the data is considered missing
-  # and will be tossed out
-  toss = np.isinf(s)
-  z = z[~toss] 
-  d = d[~toss]
-  s = s[~toss]
-  p = p[~toss]
+  # expand so that there is a covariance matrix and basis functions
+  # for each station that has sufficient data. 
+  noise_sigma = np.zeros((Nt,Nx,Nt,Nx))
+  noise_p = np.zeros((Nt,Nx,Np,len(good_stations)))
+  for i,j in enumerate(good_stations):
+    noise_sigma[:,j,:,j] = sta_sigma
+    noise_p[:,j,:,i] = sta_p
+    
+  noise_sigma = noise_sigma.reshape((Nt*Nx,Nt*Nx))
+  noise_p = noise_p.reshape((Nt*Nx,Np*len(good_stations)))
 
-  prior_gp = gpcomp(prior_model,prior_params)
-  noise_gp = gpcomp(noise_model,noise_params)    
-  full_gp  = prior_gp + noise_gp 
+  # add spatial noise
+  noise_sigma += noise_gp.covariance(z,z)
+  noise_p = np.hstack((noise_p,noise_gp.basis(z)))
 
-  full_sigma = full_gp.covariance(z,z) # model covariance
-  full_mu = full_gp.mean(z) # model mean
-  full_p  = np.hstack((full_gp.basis(z),p))
-  toss = _is_outlier(d,s,full_sigma,full_mu,full_p,tol)
-  logger.info('Detected %s outliers out of %s observations' % (sum(toss),len(toss)))
-  z = z[~toss] 
-  d = d[~toss]
-  s = s[~toss]
-  p = p[~toss]
+  prior_sigma = prior_gp.covariance(z,z)
+  prior_p = prior_gp.basis(z)
 
-  noise_sigma = np.diag(s**2) + noise_gp.covariance(z,z)
-  noise_p     = np.hstack((noise_gp.basis(z),p))
+  full_sigma = noise_sigma + prior_sigma
+  full_p = np.hstack((noise_p,prior_p))
+  full_mu = np.zeros(len(z))  
+
+  # returns the indices of outliers 
+  outliers,fit = _is_outlier(d.ravel(),sd.ravel(),full_sigma,full_mu,full_p,tol)
+  fit = fit.reshape((Nt,Nx))
+  # record removed data
+  d.ravel()[outliers] = np.nan
+  sd.ravel()[outliers] = np.inf
+  removed.ravel()[outliers] = True
+  
+  # toss out stations that are outliers or have infinite uncertainty
+  toss = np.isinf(sd.ravel()) 
+  z,d,sd = z[~toss],d.ravel()[~toss],sd.ravel()[~toss]   
+  noise_p = noise_p[~toss]
+  noise_sigma = noise_sigma[np.ix_(~toss,~toss)]
+  # add formal data uncertainties to the noise covariance matrix
+  noise_sigma += np.diag(sd**2)
+  
   # condition the prior with the data
   post_gp = prior_gp.condition(z,d,sigma=noise_sigma,p=noise_p)
   dx_gp = post_gp.differentiate((1,1,0)) # x derivative of velocity
   dy_gp = post_gp.differentiate((1,0,1)) # y derivative of velocity
-  if return_sample:
-    out = []
-    out += post_gp.sample(out_z)
-    out += np.zeros(out_z.shape[0])
-    out += dx_gp.sample(out_z)
-    out += np.zeros(out_z.shape[0])
-    out += dy_gp.sample(out_z)
-    out += np.zeros(out_z.shape[0])
-    
-  else:
-    out  = []
-    out += post_gp.meansd(out_z)
-    out += dx_gp.meansd(out_z)
-    out += dy_gp.meansd(out_z)
 
-  # reshape output arrays
-  out = [i.reshape((out_t.shape[0],out_x.shape[0])) for i in out]
+  dx,sdx = dx_gp.meansd(out_z)
+  dx = dx.reshape((out_t.shape[0],out_x.shape[0]))
+  sdx = sdx.reshape((out_t.shape[0],out_x.shape[0]))
+
+  dy,sdy = dy_gp.meansd(out_z)
+  dy = dy.reshape((out_t.shape[0],out_x.shape[0]))
+  sdy = sdy.reshape((out_t.shape[0],out_x.shape[0]))
+
+  out  = (removed,fit,dx,sdx,dy,sdy)
   return out